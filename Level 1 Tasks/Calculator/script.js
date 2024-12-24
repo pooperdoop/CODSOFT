d37e6158394b95dacd21512ce520f1b990fa5666
Bustah <psnatb2lme@gmail.com>
2024-12-24 15:18:02 +0800

    minor tweaks

--- Level 1 Tasks/Calculator/script.js
+++ Level 1 Tasks/Calculator/script.js
@@ -1,6 +1,6 @@
 const screen = document.getElementById('screen');
 function enterNum(n){
-
+CleanUp();
     if(typeof n === "number"){
         if(screen.value === '0'){
             screen.value = n;
@@ -12,24 +12,34 @@ function enterNum(n){
 
 function enterOperation(n){
     let lastChar = screen.value.slice(-1);
- if(n != '='){
-    if(n == 'D'){
-        screen.value = screen.value.substring(0, screen.value.length-1);
+    if(screen.value == null || screen.value == '0'|| screen.value == ''){
+        return;
+    } else{
+        if(n != '='){
+            if(n == 'D'){
+                screen.value = screen.value.substring(0, screen.value.length-1);
+            }
+            else if(lastChar == '÷' || lastChar == '×'|| lastChar == '-'|| lastChar == '+'){
+                screen.value = screen.value.substring(0, screen.value.length-1) + n;
+            }
+            else{
+                screen.value+=n;
+            }
+        }
+        else{
+            try{
+                screen.value = screen.value.replaceAll('÷', '/');
+                screen.value = screen.value.replaceAll('×', '*');
+                screen.value = eval(screen.value);
+            } catch(e){
+                screen.value = 'Error';
+            }
+        }
     }
-    else if(lastChar == '÷' || lastChar == '×'|| lastChar == '-'|| lastChar == '+'){
-        screen.value = screen.value.substring(0, screen.value.length-1) + n;
+}
+
+function CleanUp(){
+    if(screen.value == "Error"){
+        screen.value = "";
     }
-    else{
-        screen.value+=n;
-    }
-  }
-  else{
-    try{
-        screen.value = screen.value.replaceAll('÷', '/');
-        screen.value = screen.value.replaceAll('×', '*');
-        screen.value = eval(screen.value);
-    } catch(e){
-        screen.value = 'Error';
-    }
-  }
 }
