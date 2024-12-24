75ae3be3547ad3c8fff3f6da8e6ee2a230386eab
Bustah <psnatb2lme@gmail.com>
2024-12-24 15:06:46 +0800

    Level 1 - Task 3 - Calculator

--- Level 1 Tasks/Calculator/script.js
+++ Level 1 Tasks/Calculator/script.js
@@ -1,307 +1,35 @@
-var calculation = ""; // saves value of field when needed
-var periodCheck = false; // checks if period exists within a number
-var parCheck = false; // checks whether open or close parenthesis should be placed
-var parCount = 0; // number of open parenthesis within the equation
-var currentEquation; // takes the first part of the equation if parentheses are involved
-var currentEquationAnswer; // answer to the first equation
-var currentAns = ""; // the current answer to the field
+const screen = document.getElementById('screen');
+function enterNum(n){
 
-
-function EnterInput(input){
-    Cleanup();
-    let lastChar = document.getElementById("enterfield").value.slice(-1);
-
-    if (typeof input === 'number' && input >= 0 && input <= 9) {
-        // Handle numbers
-        if(lastChar == ')'){
-            appendToField('×');
-        }
-        appendToField(input);
-        parCheck = true;
-    } else if (input === 'P') {
-        // Handle parentheses 
-        // Handles parenthesis calculation
-        if (!parCheck) {
-            if(calculation == ""){
-            }else{
-                if(lastChar >= 0 && lastChar <= 9 || lastChar == ')'){
-                    appendToField('×');
-                    }
-            }
-            appendToField('(');
-            parCount++;
-
-        } else {
-            appendToField(')');
-            parCount--;
-        }
-
-    } else if (input === '.') {
-        // Handle decimal point
-        if (!periodCheck) {
-            appendToField('.');
-            periodCheck = true;
-        }
-        parCheck = false;
-    } else if(input ==='√'){
-        if(calculation == ""){
-        }else{
-            if(lastChar >= 0 && lastChar <= 9 || lastChar == ')'){
-            appendToField('×');
-            }
-        }
-        appendToField('√(');
-        parCount++;
-    }
-    checkforCalculation();
-    calculation = document.getElementById('enterfield').value;
-    // saves input field value to be used in different functions (deleting and clearing)
-    CheckForPar();
-}
-
-function EnterAction(n){
-    Cleanup();  
-    let calculationfield = document.getElementById('enterfield').value;
-    switch (n){
-        case 'D':
-            CheckForPar(); 
-            let lastChar = document.getElementById("enterfield").value.slice(-1);
-            let lastTwoChar = document.getElementById("enterfield").value.slice(-2);
-            if(lastChar == "."){
-                periodCheck = false;
-            }
-            if(lastChar == "("){
-                parCount--;
-            }
-            if(lastChar == ")"){
-                parCount++;
-                if(parCount > 0){
-                    parCheck = true;
-                }
-            }
-            if(lastTwoChar === "√("){
-                del = calculationfield.substring(0, calculationfield.length-2); 
-            } else{
-            del = calculationfield.substring(0, calculationfield.length-1); 
-            }
-            document.getElementById('enterfield').value = del;
-            calculation = document.getElementById('enterfield').value;
-
-            if(calculation.slice(-1) == "("){
-
-                parCheck = false;
-            }
-            checkforCalculation();
-            break;
-        case 'C':
-            document.getElementById('enterfield').value = "";
-            calculation = "";
-            periodCheck = false;
-            parCheck = false;
-            parCount = 0;
-            currentEquation = "";
-            currentEquationAnswer  = "";
-            currentAns = "";
-            break;
-            // effectively, a reset to the inputs and values of vars
-
-    }
-}
-
-function EnterOperator(n){
-    checkforCalculation();
-    Cleanup();  
-    let enterfield = document.getElementById('enterfield').value;
-    if(n != "="){
-        if(enterfield.length == 0 && n != "-" || enterfield == "-"){
-            // returns if input is any operator except - or if inputfield only consists of a -
-            return;
+    if(typeof n === "number"){
+        if(screen.value === '0'){
+            screen.value = n;
         } else{
-            let lastChar = enterfield.slice(-1);
-            // getst the last character
-            if(lastChar == "+" || lastChar == "-" || lastChar == "÷" || lastChar =="×" || lastChar =="%" || lastChar =="^"){
-
-                let del = enterfield.substring(0, enterfield.length-1); 
-                changeFieldValue(del +n);
-                // removes previous operator and replaces it with the new operator inputted 
-
-            }else{
-                if(lastChar == "."){
-                   appendToField("0" + n);
-                } else{
-                   appendToField(n);  
-                }
-                if(n === "^"){
-                }else{
-                    CheckForPar();
-                }
-                periodCheck = false;
-
-            }   // Handles period operator reiteration 
+            screen.value += n;
         }
+    }
+}
+
+function enterOperation(n){
+    let lastChar = screen.value.slice(-1);
+ if(n != '='){
+    if(n == 'D'){
+        screen.value = screen.value.substring(0, screen.value.length-1);
+    }
+    else if(lastChar == '÷' || lastChar == '×'|| lastChar == '-'|| lastChar == '+'){
+        screen.value = screen.value.substring(0, screen.value.length-1) + n;
     }
     else{
-         if(document.getElementById('enterfield').value == ""){
-            // prevents any output if enterfield is empty
-            return;
-         }else{
-            currentEquationAnswer = calculation;
-
-        while(parCount != 0){
-
-            currentEquationAnswer += ")";
-            parCount--;
-
-        }   
-            CheckForMathFunction(currentEquationAnswer);
-            changeFieldValue(currentAns);
-            parCheck = false;
-            parCount = 0;
-        }
+        screen.value+=n;
     }
-}
-
-function CheckForPar(){
-    const enterField = document.getElementById('enterfield');
-    if(parCount == 0){
-        parCheck = false;            
-        currentEquation = enterField.value;
-        currentEquationAnswer = currentEquation;
-        CheckForMathFunction(currentEquationAnswer);
-    }
-    // checks if there are any parenthesis in the inputfield
-
-}
-function checkforCalculation(){
-    if(calculation.length == 0){
-        calculation = "";
-    }
-}
-
-function appendToField(value){
-
-    // Gets current value of the input field
-    const enterField = document.getElementById('enterfield');
-    // appends input to current value 
-    enterField.value += value;
-}
-
-function changeFieldValue(value){
-    const enterField = document.getElementById('enterfield');
-    enterField.value = value;
-
-    if(enterfield.value.includes('.')){
-        periodCheck = true;
-    }else{
-        periodCheck =false;
-    }
-}
-
-function Cleanup(){
-    const enterField = document.getElementById('enterfield');
-
-    if(enterField.value == "Format Error"){
-        enterField.value ="";
-        calculation = "";
-    }
-}
-
-function CheckForMathFunction(currentValue){
-    if(currentValue.includes("√")){
-        SquareRootFunction();
-    }
-    else if(currentValue.lastIndexOf("^") !== -1){
-        ExponentFunction();
-    }  else{
-
-        currentValue = currentValue.replaceAll("÷", "/")
-        currentValue = currentValue.replaceAll("×", "*")
-        //Replaces characters with equation friendly operators
-        try {
-            currentValue = eval(currentValue);
-            currentAns = currentValue;
-            console.log(currentAns);
-        } catch (e) {
-            currentAns = "Format Error";
-        }
-
-    }
-}
-
-function SquareRootFunction(){
-    let startChar = currentEquationAnswer.lastIndexOf("√") + 2;
-    let parenthesesCount = 1;
-    let sqrRtEquation = "(";
-
-    while(parenthesesCount != 0){
-
-        let currentChar = currentEquationAnswer.charAt(startChar);
-        sqrRtEquation += currentChar;
-
-        if(currentChar === "("){
-            parenthesesCount++;
-        } else if(currentChar === ")"){
-            parenthesesCount--;
-        }
-        startChar++;
-    }
-    console.log(sqrRtEquation);
-    let removable = "√"+sqrRtEquation;
-
-    sqrRtEquation = sqrRtEquation.replaceAll("÷", "/")
-    sqrRtEquation = sqrRtEquation.replaceAll("×", "*")
-
+  }
+  else{
     try{
-    let evalResult = eval(sqrRtEquation);
-    let finalSqrrt = Math.sqrt(evalResult);
-    currentEquationAnswer = currentEquationAnswer.replace(removable, finalSqrrt);
-    
-    console.log(evalResult);
-    console.log(finalSqrrt);
-    console.log(currentEquationAnswer);
+        screen.value = screen.value.replaceAll('÷', '/');
+        screen.value = screen.value.replaceAll('×', '*');
+        screen.value = eval(screen.value);
     } catch(e){
-        currentEquationAnswer = "Format Error";
+        screen.value = 'Error';
     }
-
-    CheckForMathFunction(currentEquationAnswer);
-
-}
-
-function ExponentFunction(){
-    let startChar = currentEquationAnswer.lastIndexOf("^")+1;
-    let reverseStartChar = currentEquationAnswer.lastIndexOf("^")-1;
-    let power ="";
-    let num ="";
-    let regex= /[0-9\()]/;
-    while(regex.test(currentEquation.charAt(startChar))){
-        let currentChar = currentEquationAnswer.charAt(startChar);
-        power += currentChar;
-        startChar++;
-    }
-    while(regex.test(currentEquation.charAt(reverseStartChar))){
-        let currentChar = currentEquationAnswer.charAt(reverseStartChar);
-        num += currentChar;
-        reverseStartChar--;
-    }
-
-    num = num.split("").reverse().join("");
-    let removable = num+"^"+power;
-    console.log(removable);
-
-    // sqrRtEquation = sqrRtEquation.replaceAll("÷", "/")
-    // sqrRtEquation = sqrRtEquation.replaceAll("×", "*")
-
-    // try{
-    // let evalResult = eval(sqrRtEquation);
-    let finalPow = Math.pow(num, power);
-    currentEquationAnswer = currentEquationAnswer.replace(removable, finalPow);
-    
-    // console.log(evalResult);
-    // console.log(finalSqrrt);
-    // console.log(currentEquationAnswer);
-    // } catch(e){
-    //     currentEquationAnswer = "Format Error";
-    // }
-
-     CheckForMathFunction(currentEquationAnswer);
+  }
 }
